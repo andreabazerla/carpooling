0ec3fcbccb1057476eabc569aeaee73e6cab30a6
Andrea Bazerla <andrea.bazerla@gmail.com>
2021-11-07 00:43:44 +0100

    Created Main terminal

---
+++ Main.py
@@ -0,0 +1,130 @@
+import os
+
+from Generator import Generator
+
+clear = lambda: os.system('clear')
+
+clear()
+
+instance_generated = False
+
+menu_options = {
+    1: 'Generate new instance',
+    2: 'Show instance data',
+    3: 'Exit',
+}
+
+menu_options_show_instance = {
+    1: 'Show distance histogram',
+    2: 'Show angle histogram',
+    3: 'Show polar coordinates',
+    4: 'Show cartesian coordinates',
+    5: 'Show graph',
+    6: 'Show map',
+    0: '<- Back',
+}
+
+def print_menu():
+    print('Menu')
+    
+    for key in menu_options.keys():
+        if key == 2 and instance_generated == False:
+            continue
+        print(str(key) + ')', menu_options[key])
+    print()
+
+def print_menu_show_instance():
+    print('Show instance')
+    
+    for key in menu_options_show_instance.keys():
+        print(str(key) + ')', menu_options_show_instance[key])
+    print()
+
+def generate_instance():
+    global instance_generated, generator
+    clear()
+    
+    print('Insert instance data\n')
+
+    origin_latitude = float(input('Latitude of origin (44.83436): ') or 44.83436)
+    origin_longitude = float(input('Longitude of origin (11.59934): ') or 11.59934)
+
+    ORIGIN = (origin_latitude, origin_longitude)
+
+    students_number = int(input('Insert students number (100): ') or 100)
+    drivers_percentage = float(input('Insert drivers percentage (12): ') or 12)
+    mean = float(input('Insert students distribution mean (1000): ') or 1000)
+    sd = float(input('Insert students distribution standard deviation (5000): ') or 5000)
+    low = float(input('Insert students truncated distribution lower limit (1000): ') or 1000)
+    upp = float(input('Insert students truncated distribution upper limit (10000): ') or 10000)
+
+    generator = Generator(ORIGIN, students_number, drivers_percentage, mean, sd, low, upp)
+
+    students_coordinates = generator.get_students_coordinates()
+
+    drivers_coordinates = generator.get_drivers_coordinates(students_coordinates)
+    passengers_coordinates = generator.get_passengers_coordinates(students_coordinates)
+
+    instance_generated = True
+
+def show_instance():
+    clear()
+
+print('Operative Research - A.A. 2020/21\n')
+print('Andrea Bazerla - 151792')
+print('Taoufik Soudi - 124485')
+print()
+
+if __name__=='__main__':
+    while(True):
+        print_menu()
+        
+        menu_option = ''
+        
+        try:
+            menu_option = int(input('Enter your choice: '))
+        except:
+            print('Wrong input. Please enter a valid number...')
+
+        if menu_option == 1:
+            generate_instance()
+            clear()
+
+            print('Instance created :)\n')
+            continue
+        if menu_option == 2:
+            while(True):
+                clear()
+                print_menu_show_instance()
+                
+                menu_show_instance_option = ''
+                
+                try:
+                    menu_show_instance_option = int(input('Enter your choice: '))
+                except:
+                    print('Wrong input. Please enter a valid number...')
+
+                if menu_show_instance_option == 1:
+                    generator.show_distances_distribution()
+                elif menu_show_instance_option == 2:
+                    generator.show_angles_distribution()
+                elif menu_show_instance_option == 3:
+                    generator.show_polar_coordinates()
+                elif menu_show_instance_option == 4:
+                    generator.show_cartesian_coordinates()
+                elif menu_show_instance_option == 5:
+                    pass
+                elif menu_show_instance_option == 6:
+                    pass
+                elif menu_show_instance_option == 0:
+                    clear()
+                    break
+                else:
+                    clear()
+                    print('Invalid option. Please enter a valid number...')
+                
+        elif menu_option == 3:
+            exit()
+        else:
+            clear()
+            print('Invalid option. Please enter a valid number...')
